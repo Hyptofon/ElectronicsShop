51a3084e16bbed55e80dd7ebbc8441688e7a5a25
Hyptofon <hyptofonhyptofon@gmail.com>
2025-11-17 09:30:42 +0200

    refactor

---
+++ src/Infrastructure/Persistence/Migrations/20251117070722_AddRowVersionToUser.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddRowVersionToUser : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<byte[]>(
+                name: "row_version",
+                table: "users",
+                type: "bytea",
+                rowVersion: true,
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "row_version",
+                table: "users");
+        }
+    }
+}
